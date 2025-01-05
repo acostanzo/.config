949d9c2b2cd7fae2bbf5f5c4fa8bae68bdcf0d45
acostanzo <mail@acostanzo.com>
2025-01-05 15:00:34 -0500

    Initial nvim config
    - Based lazyvim.org
    - Customizations:
      - catppuccin theme
      - oil
      - no swapfiles
      - singular status line

---
+++ nvim/lua/plugins/oil.lua
@@ -0,0 +1,8 @@
+return {
+  {
+    "stevearc/oil.nvim",
+    opts = {},
+    keys = { { "<leader>h", "<cmd>Oil<cr>", desc = "Oil " } },
+    dependencies = { { "echasnovski/mini.icons", opts = {} } },
+  },
+}
