09f7db4dd63e32530eb6520a6e8c969430e89611
acostanzo <mail@acostanzo.com>
2025-01-06 10:47:37 -0500

    Source aliases to apply overrides such as ll

--- fish/config.fish
+++ fish/config.fish
@@ -5,4 +5,5 @@ set -x EDITOR nvim
 if status is-interactive
     # Commands to run in interactive sessions can go here
     eval "$(/opt/homebrew/bin/brew shellenv)"
+    source ~/.config/fish/functions/aliases.fish
 end
