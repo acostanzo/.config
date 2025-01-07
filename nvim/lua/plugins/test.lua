ead12ead3def684a703e7ec6356926f58a0ad0ff
acostanzo <mail@acostanzo.com>
2025-01-07 11:44:22 -0500

    Enable testing in lazyvim

---
+++ nvim/lua/plugins/test.lua
@@ -0,0 +1,13 @@
+return {
+  { "nvim-neotest/neotest-jest" },
+  { "olimorris/neotest-rspec" },
+  {
+    "nvim-neotest/neotest",
+    opts = {
+      adapters = {
+        "neotest-jest",
+        "neotest-rspec",
+      },
+    },
+  },
+}
