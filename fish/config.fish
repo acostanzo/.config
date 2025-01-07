8839928f481564f8a543f970330e9df1a43ac9a6
acostanzo <mail@acostanzo.com>
2025-01-07 09:45:23 -0500

    Add Starship prompt

--- fish/config.fish
+++ fish/config.fish
@@ -1,9 +1,12 @@
 set -g fish_key_bindings fish_vi_key_bindings
 set -x GPG_TTY (tty)
 set -x EDITOR nvim
+set -x GPG_TTY (tty)
+set -x STARSHIP_CONFIG ~/.config/starship/starship.toml
 
 if status is-interactive
     # Commands to run in interactive sessions can go here
     eval "$(/opt/homebrew/bin/brew shellenv)"
     source ~/.config/fish/functions/aliases.fish
 end
+starship init fish | source
