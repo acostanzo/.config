6bb0c15e81720004c30f7abbf3c2bac7b85ada58
acostanzo <mail@acostanzo.com>
2025-01-09 10:46:13 -0500

    Fix nvim theme

--- nvim/lua/plugins/catppuccin.lua
+++ nvim/lua/plugins/catppuccin.lua
@@ -3,10 +3,8 @@ return {
     "catppuccin/nvim",
     lazy = false,
     name = "catppuccin",
-    config = function()
-      require("catppuccin").setup({
-        flavour = "frappe",
-      })
-    end,
+    opts = {
+      flavour = "frappe",
+    },
   },
 }
